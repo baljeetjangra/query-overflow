9f6fc3cf3ea8612f5b5cd5d63ed5ac0a605fc821
Baljeet <baljeetjangraa@gmail.com>
2023-09-30 22:18:31 +0530

    Home page created

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -13,22 +13,22 @@ const RightSideBar = () => {
 
   const popularTags = [
     {
-      _id: 1,
+      _id: "1",
       name: "React",
       totalQuestions: 3,
     },
     {
-      _id: 2,
+      _id: "2",
       name: "Javascript",
       totalQuestions: 1,
     },
     {
-      _id: 3,
+      _id: "3",
       name: "Redux",
       totalQuestions: 3,
     },
     {
-      _id: 4,
+      _id: "4",
       name: "Typescript",
       totalQuestions: 5,
     },

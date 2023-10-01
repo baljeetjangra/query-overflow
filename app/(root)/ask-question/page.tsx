e65c323feea3436b14c88d013fd375fac4760a66
Baljeet <baljeetjangraa@gmail.com>
2023-10-01 21:18:19 +0530

    Question form and home page connected to backend

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,24 @@
+import Question from "@/components/shared/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+const AskQuestion = async () => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/");
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;

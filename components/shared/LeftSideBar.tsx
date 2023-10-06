6d74d9997bbe6f349059bdad37c2c464e3871e5f
Baljeet <baljeetjangraa@gmail.com>
2023-10-06 06:35:56 +0530

    profile page added

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedOut, auth } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 
 const LeftSideBar = () => {
   const pathname = usePathname();
+  const { userId } = auth();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
@@ -16,6 +17,15 @@ const LeftSideBar = () => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
+
+          if (item.route === "/profile") {
+            if (userId) {
+              item.route = `/profile/${userId}`;
+            } else {
+              return null;
+            }
+          }
+
           return (
             <Link
               key={item.route}
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async () => {
   }
 
   if (isConnected) {
-    return console.log("Mongoose is already connected");
+    return;
   }
 
   try {

3a001c86471c93045a24968c111c30122f87d801
Baljeet <baljeetjangraa@gmail.com>
2023-10-15 11:23:40 +0530

    hot qsn and popular tags added

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -2,53 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const RightSideBar = () => {
-  const hotQuestios = [
-    { _id: 1, title: "How to create a new project in Figma?" },
-    { _id: 2, title: "How to deploy a website on vercel?" },
-    { _id: 3, title: "How to create a new project in Figma?" },
-    { _id: 4, title: "How to create a new project in Figma?" },
-  ];
-
-  const popularTags = [
-    {
-      _id: "1",
-      name: "React",
-      totalQuestions: 3,
-    },
-    {
-      _id: "2",
-      name: "Javascript",
-      totalQuestions: 1,
-    },
-    {
-      _id: "3",
-      name: "Redux",
-      totalQuestions: 3,
-    },
-    {
-      _id: "4",
-      name: "Typescript",
-      totalQuestions: 5,
-    },
-  ];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
-      <div className="">
+      <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestios.map((item) => (
+          {hotQuestions.map((question: any) => (
             <Link
-              href={`/questions/${item._id}`}
-              key={item._id}
+              href={`/question/${question._id}`}
+              key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
-              <p className="body-medium text-dark500_light700">{item.title}</p>
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
               <Image
                 src="/assets/icons/chevron-right.svg"
-                alt="chevron-right"
+                alt="chevron right"
                 width={20}
                 height={20}
                 className="invert-colors"
@@ -60,12 +37,12 @@ const RightSideBar = () => {
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map((item) => (
+          {popularTags.map((tag) => (
             <RenderTag
-              key={item._id}
-              _id={item._id}
-              name={item.name}
-              totalQuestions={item.totalQuestions}
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
@@ -75,4 +52,4 @@ const RightSideBar = () => {
   );
 };
 
-export default RightSideBar;
+export default RightSidebar;
